fbc036384f632882d352e73d4a322d896a0707ad
JuanFig023 <juanseb023@gmail.com>
2025-03-11 11:58:02 -0500

    added CRUD and test

--- index.test.js
+++ index.test.js
@@ -19,4 +19,23 @@ describe('./musicians endpoint', () => {
         expect(response.ok).toBe(true);
         expect(response.type).toBe("application/json");
     })
+    test("Testing /musicians/:id endpoint", async () => {
+        const response = await request(app).get("/musicians/1");
+        expect(response.ok).toBe(true);
+        expect(response.type).toBe("application/json");
+    })
+    test("Testing /musicians POST endpoint", async () => {
+        const response = await request(app).post("/musicians");
+        expect(response.ok).toBe(true);
+        expect(response.type).toBe("application/json");
+    })
+    test("Testing /musicians PUT endpoint", async () => {
+        const response = await request(app).put("/musicians/1");
+        expect(response.ok).toBe(true);
+        expect(response.type).toBe("application/json");
+    })
+    test("Testing /musicians DELETE endpoint", async () => {
+        const response = await request(app).delete("/musicians/1");
+        expect(response.ok).toBe(true);
+    })
 })
--- src/app.js
+++ src/app.js
@@ -11,9 +11,28 @@ app.get("/musicians", async (req,res) => {
     res.json(musicians);
 })
 
+app.get("/musicians/:id", async (req,res) => {
+    const musicianPerId = await Musician.findByPk(req.params.id);
+    res.json(musicianPerId);
+})
 
+app.use(express.json());
+app.use(express.urlencoded({ extended:true}));
 
+app.post("/musicians", async (req,res) => {
+    const newMusician = await Musician.create(req.body);
+    res.json(newMusician);
+})
 
+app.put("/musicians/:id", async (req,res) => {
+    const musicianUpdated = await Musician.findByPk(req.params.id);
+    await musicianUpdated.update(req.body);
+    res.json(musicianUpdated);
+})
 
+app.delete("/musicians/:id", async (req,res) => {
+    const musicianDeleted = await Musician.findByPk(req.params.id);
+    await musicianDeleted.destroy()
+})
 
 module.exports = app;
